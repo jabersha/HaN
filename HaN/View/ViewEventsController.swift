d54765119947a209d32805264999eba8a7d5b65a
Jaber Shamali <jaber_sha@hotmail.com>
2022-05-01 05:48:24 -0300

    create/add elements screen events

--- HaN/View/ViewEventsController.swift
+++ HaN/View/ViewEventsController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import Foundation
+
 
 class ViewEventsController: UIViewController {
 
-    let viewLogin = ViewLogin()
+    let viewEvents = ViewEvents()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
@@ -17,18 +20,78 @@ class ViewEventsController: UIViewController {
 
 }
 
+class ViewEvents : UIView {
+    
+    override init(frame: CGRect = .zero){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    lazy var logoImg: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        view.image = UIImage(named: "han")
+        return view
+    }()
+    
+    
+    lazy var newEventBtn: UIButton = {
+        let view = UIButton(frame: .zero)
+        view.backgroundColor = .darkGray
+        view.setTitle("+", for: .normal)
+        view.titleLabel?.font = UIFont(name: "Arial", size: 90)
+        
+        return view
+    }()
+    
+    let tableEvents : UITableView = {
+        let tableView = UITableView()
+        tableView.indicatorStyle = .default
+        tableView.backgroundColor = .systemGray5
+        tableView.layer.cornerRadius = 20.0
+        tableView.clipsToBounds = true
+//            tableView.register(NotaNegocicaoCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+}
+
 
 extension ViewEventsController: CodeView {
     func buildHierarchy() {
-        
+        view.addSubview(viewEvents)
+        view.addSubview(viewEvents.logoImg)
+        view.addSubview(viewEvents.tableEvents)
+        view.addSubview(viewEvents.newEventBtn)
     }
     
     func configConstraints() {
         
+        viewEvents.logoImg.snp.makeConstraints{ make in
+            make.top.equalToSuperview().offset(35)
+            make.height.width.equalTo(60)
+            make.centerX.equalToSuperview()
+        }
+        
+        viewEvents.tableEvents.snp.makeConstraints{ make in
+            make.top.equalTo(viewEvents.logoImg.snp.bottom).offset(10)
+            make.bottom.equalTo(viewEvents.newEventBtn.snp.top).offset(15)
+            make.left.right.equalToSuperview().offset(20).inset(20)
+        }
+        
+        viewEvents.newEventBtn.snp.makeConstraints{ make in
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(20)
+            make.centerX.equalToSuperview()
+            make.height.width.equalTo(55)
+        }
+
+        
     }
     
     func additionalConfig() {
-        
+        view.backgroundColor = UIColor.init(hexString: "#cbcbc9")
     }
     
     
---
+++ HaN/View/ViewLoginController.swift
@@ -0,0 +1,161 @@
+//
+//  ViewLoginController.swift
+//  HaN
+//
+//  Created by Jaber Shamali on 01/05/22.
+//
+
+
+import UIKit
+
+class ViewLoginController: UIViewController {
+
+    let viewLogin = ViewLogin()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    
+    func nextView(){
+        dismiss(animated: true, completion: nil)
+        let vc = ViewEventsController()
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .coverVertical
+        self.present(vc, animated: true, completion: nil)
+        
+    }
+
+
+}
+
+
+class ViewLogin: UIView{
+    
+    
+    override init(frame: CGRect = .zero){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var logoImg: UIImageView = {
+        let view = UIImageView(frame: .zero)
+        view.image = UIImage(named: "han")
+        view.layer.cornerRadius = 20.0
+        view.clipsToBounds = true
+        
+        return view
+    }()
+    
+    lazy var userTxtFd: UITextField = {
+        let view = UITextField(frame: .zero)
+        view.keyboardType = .emailAddress
+        view.layer.cornerRadius = 10.0
+        view.clipsToBounds = true
+        view.placeholder = "Login"
+        view.textAlignment = .center
+        view.backgroundColor = .systemGray3
+        
+        return view
+    }()
+    
+    lazy var passTxtFd: UITextField = {
+        let view = UITextField(frame: .zero)
+        view.isSecureTextEntry = true
+        view.layer.cornerRadius = 10.0
+        view.clipsToBounds = true
+        view.placeholder = "Senha"
+        view.textAlignment = .center
+        view.backgroundColor = .systemGray3
+        
+        return view
+    }()
+    
+    lazy var userRecoveryBtn: UIButton = {
+        let view = UIButton(frame: .zero)
+        view.setTitle("Esqueceu a senha?", for: .normal)
+        view.setTitleColor( UIColor.darkGray , for: .normal)
+        
+        return view
+    }()
+    
+    lazy var enterBtn: UIButton = {
+        let view = UIButton(frame: .zero)
+        view.backgroundColor = .darkGray
+        view.layer.cornerRadius = 10.0
+        view.clipsToBounds = true
+        view.setTitle("Entrar", for: .normal)
+        view.setTitleColor( UIColor.white , for: .normal)
+        view.addTarget(self, action: #selector(buttonAct), for: .touchUpInside)
+
+        return view
+    }()
+    
+    
+    @objc func buttonAct(sender: UIButton!){
+        let vc = ViewLoginController()
+        vc.nextView()
+        
+    }
+
+}
+
+extension ViewLoginController : CodeView{
+    func buildHierarchy() {
+        view.addSubview(viewLogin)
+        view.addSubview(viewLogin.logoImg)
+        view.addSubview(viewLogin.userTxtFd)
+        view.addSubview(viewLogin.passTxtFd)
+        view.addSubview(viewLogin.userRecoveryBtn)
+        view.addSubview(viewLogin.enterBtn)
+
+    }
+    
+    func configConstraints() {
+        viewLogin.logoImg.snp.makeConstraints{ make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(70)
+            make.centerX.equalToSuperview()
+            
+        }
+        
+        viewLogin.userTxtFd.snp.makeConstraints{ make in
+            make.top.equalTo(viewLogin.logoImg.snp.bottom).offset(70)
+            make.centerX.equalTo(viewLogin.logoImg)
+            make.width.equalTo(190)
+            make.height.equalTo(30)
+        }
+        
+        viewLogin.passTxtFd.snp.makeConstraints{ make in
+            make.top.equalTo(viewLogin.userTxtFd.snp.bottom).offset(30)
+            make.centerX.equalTo(viewLogin.userTxtFd)
+            make.left.right.equalTo(viewLogin.userTxtFd)
+            make.height.equalTo(30)
+        }
+        
+        viewLogin.userRecoveryBtn.snp.makeConstraints{ make in
+            make.top.equalTo(viewLogin.passTxtFd.snp.bottom).offset(0)
+            make.left.right.equalTo(viewLogin.passTxtFd)
+            make.height.equalTo(30)
+        }
+        
+        viewLogin.enterBtn.snp.makeConstraints{ make in
+            make.top.equalTo(viewLogin.userRecoveryBtn.snp.bottom).offset(30)
+            make.centerX.equalTo(viewLogin.userTxtFd)
+            make.left.right.equalTo(viewLogin.passTxtFd)
+            make.height.equalTo(50)
+        }
+    }
+    
+    func additionalConfig() {
+        view.backgroundColor = .gray
+
+    }
+    
+    
+}
+
+
